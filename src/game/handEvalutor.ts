85672489a84bc54481f76cc65ae09c667aedce98
YashVar304 <yashvarshney304@gmail.com>
2026-08-16 18:05:30 +0530

    feat: enhance winner determination logic in determineWinner function for various combinations

--- src/game/handEvalutor.ts
+++ src/game/handEvalutor.ts
@@ -70,6 +70,72 @@ export const winingCombinationPriority = ['Trail', 'Pure Sequence', 'Sequence', 
                 console.log("Player 4 wins!")
                 return { winner: 4, amount: 0 }
             } else {
+                if(player1Combination === 'Trail') {
+                    const player1RankValue = player1Cards[0].rank === 'A' ? 14 : player1Cards[0].rank === 'K' ? 13 : player1Cards[0].rank === 'Q' ? 12 : player1Cards[0].rank === 'J' ? 11 : parseInt(player1Cards[0].rank);
+                    const player4RankValue = player4Cards[0].rank === 'A' ? 14 : player4Cards[0].rank === 'K' ? 13 : player4Cards[0].rank === 'Q' ? 12 : player4Cards[0].rank === 'J' ? 11 : parseInt(player4Cards[0].rank);
+
+                    if (player1RankValue > player4RankValue) {
+                        console.log("Dealer wins!")
+                        return { winner: 1, amount: 0 }
+                    } else if (player1RankValue < player4RankValue) {
+                        console.log("Player 4 wins!")
+                        return { winner: 4, amount: 0 }
+                    } else {
+                        console.log("It's a tie!")
+                        return { winner: null, amount: 0 }
+                    }
+                }
+                if(player1Combination === 'Pure Sequence' || player1Combination === 'Sequence') {
+                    const player1RankValues = player1Cards.map(card => card.rank === 'A' ? 14 : card.rank === 'K' ? 13 : card.rank === 'Q' ? 12 : card.rank === 'J' ? 11 : parseInt(card.rank)).sort((a, b) => a - b);
+                    const player4RankValues = player4Cards.map(card => card.rank === 'A' ? 14 : card.rank === 'K' ? 13 : card.rank === 'Q' ? 12 : card.rank === 'J' ? 11 : parseInt(card.rank)).sort((a, b) => a - b);
+                    
+                    for (let i = 2; i >= 0; i--) {
+                        if (player1RankValues[i] > player4RankValues[i]) {
+                            console.log("Dealer wins!")
+                            return { winner: 1, amount: 0 }
+                        } else if (player1RankValues[i] < player4RankValues[i]) {
+                            console.log("Player 4 wins!")
+                            return { winner: 4, amount: 0 }
+                        }else {
+                            continue;
+                        }
+                    }
+                    console.log("It's a tie!")
+                    return { winner: null, amount: 0 }
+                }
+                if(player1Combination === 'Color') {
+                    const player1RankValues = player1Cards.map(card => card.rank === 'A' ? 14 : card.rank === 'K' ? 13 : card.rank === 'Q' ? 12 : card.rank === 'J' ? 11 : parseInt(card.rank)).sort((a, b) => a - b);
+                    const player4RankValues = player4Cards.map(card => card.rank === 'A' ? 14 : card.rank === 'K' ? 13 : card.rank === 'Q' ? 12 : card.rank === 'J' ? 11 : parseInt(card.rank)).sort((a, b) => a - b);
+                    
+                    for (let i = 2; i >= 0; i--) {
+                        if (player1RankValues[i] > player4RankValues[i]) {
+                            console.log("Dealer wins!")
+                            return { winner: 1, amount: 0 }
+                        } else if (player1RankValues[i] < player4RankValues[i]) {
+                            console.log("Player 4 wins!")
+                            return { winner: 4, amount: 0 }
+                        }else {
+                            continue;
+                        }
+                    }
+                    console.log("It's a tie!")
+                    return { winner: null, amount: 0 }
+                }
+            }
+            if(player1Combination === 'Pair') {
+                const player1PairRank = player1Cards.find(card => player1Cards.filter(c => c.rank === card.rank).length === 2)?.rank;
+                const player4PairRank = player4Cards.find(card => player4Cards.filter(c => c.rank === card.rank).length === 2)?.rank;
+                
+                const player1PairValue = player1PairRank === 'A' ? 14 : player1PairRank === 'K' ? 13 : player1PairRank === 'Q' ? 12 : player1PairRank === 'J' ? 11 : parseInt(player1PairRank || '0');
+                const player4PairValue = player4PairRank === 'A' ? 14 : player4PairRank === 'K' ? 13 : player4PairRank === 'Q' ? 12 : player4PairRank === 'J' ? 11 : parseInt(player4PairRank || '0');
+                if (player1PairValue > player4PairValue) {
+                    console.log("Dealer wins!")
+                    return { winner: 1, amount: 0 }
+                } else if (player1PairValue < player4PairValue) {
+                    console.log("Player 4 wins!")
+                    return { winner: 4, amount: 0 }
+                } 
+                else{
                 // If both players have the same combination, compare the highest card
                 const player1HighestCard = Math.max(...player1Cards.map(card => {
                     if (card.rank === 'A') return 14
@@ -86,7 +152,51 @@ export const winingCombinationPriority = ['Trail', 'Pure Sequence', 'Sequence', 
                     if (card.rank === 'J') return 11
                     return parseInt(card.rank)
                 }))
+                const player1TotalRankValue = player1Cards.reduce((total, card) => {
+                    if (card.rank === 'A') return total + 14
+                    if (card.rank === 'K') return total + 13
+                    if (card.rank === 'Q') return total + 12
+                    if (card.rank === 'J') return total + 11
+                    return total + parseInt(card.rank)
+                }, 0)
+                
+                const player4TotalRankValue = player4Cards.reduce((total, card) => {
+                    if (card.rank === 'A') return total + 14
+                    if (card.rank === 'K') return total + 13
+                    if (card.rank === 'Q') return total + 12
+                    if (card.rank === 'J') return total + 11
+                    return total + parseInt(card.rank)
+                }, 0)
 
+                if (player1TotalRankValue > player4TotalRankValue) {
+                    console.log("Dealer wins!")
+                    return { winner: 1, amount: 0 }
+                } else if (player1TotalRankValue < player4TotalRankValue) {
+                    console.log("Player 4 wins!")
+                    return { winner: 4, amount: 0 }
+                } else {
+                    console.log("It's a tie!")
+                    return { winner: null, amount: 0 }
+                }
+            }
+            }
+            else{
+                // If both players have the same combination, compare the highest card
+                const player1HighestCard = Math.max(...player1Cards.map(card => {
+                    if (card.rank === 'A') return 14
+                    if (card.rank === 'K') return 13
+                    if (card.rank === 'Q') return 12
+                    if (card.rank === 'J') return 11
+                    return parseInt(card.rank)
+                }))
+
+                const player4HighestCard = Math.max(...player4Cards.map(card => {
+                    if (card.rank === 'A') return 14
+                    if (card.rank === 'K') return 13
+                    if (card.rank === 'Q') return 12
+                    if (card.rank === 'J') return 11
+                    return parseInt(card.rank)
+                })) 
                 if (player1HighestCard > player4HighestCard) {
                     console.log("Dealer wins!")
                     return { winner: 1, amount: 0 }
@@ -97,6 +207,7 @@ export const winingCombinationPriority = ['Trail', 'Pure Sequence', 'Sequence', 
                     console.log("It's a tie!")
                     return { winner: null, amount: 0 }
                 }
+            
             }
         }
     }
